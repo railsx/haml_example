50b76ce6c89f8b2c9d3c109840f1498750fed5dd
Karthikeyan A K <mindaslab@gmail.com>
2013-12-26 13:01:57 +0530

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HamlExample::Application.config.secret_key_base = '6180805b90a613221f1d6b264187bfae3aafb98c82d4528e51c53fd0ceb8e6383c85d395fb07dff457c4dcaf5eece886d873dcb790edce54b6733f4c6bb01fa0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HamlExample::Application.config.session_store :cookie_store, key: '_haml_example_session'
